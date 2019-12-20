5325e3e2d44f02adc26121ac432717c398160931
Adam  Shappy <ajs11300@gmail.com>
2019-12-20 20:32:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   counter = 0
  
   while array[counter] do
-    puts array[counter]
+    if array.includes?(value_to_find)
     counter += 1
   end
 end
